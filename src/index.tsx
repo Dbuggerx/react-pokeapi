cb8cf2698d16c21a2e596aa0aaa76cf91d60ff6a
Danilo Cestari <danilofcestari@gmail.com>
2020-05-31 22:05:20 +0200

    Use HashRouter so GitHub Pages works properly

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import buildStore from "./redux/store";
 
 import App from "./App";
