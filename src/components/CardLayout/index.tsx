685fd9f90cc17f4c73b61e4353ce840e85d80386
Danilo <danilo@localhost.localdomain>
2020-10-18 21:06:00 +0200

    Improve the styles of the header component

--- src/components/CardLayout/index.tsx
+++ src/components/CardLayout/index.tsx
@@ -5,9 +5,9 @@ const CardLayout: React.FC = ({ children }) => {
   return (
     <div className="card-layout">
       <article className="card-layout__content">
-        {React.Children.map(children, child => (
-          <>{child}</>
-        ))}
+        {React.Children.map(children, child =>
+          React.cloneElement(child as React.ReactElement)
+        )}
       </article>
     </div>
   );
